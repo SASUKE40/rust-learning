e1ec820d53ffb30c36e0fac3fef1c67f043f27b5
Edward Elric <SASUKE688848@gmail.com>
2020-07-01 14:00:44 +0800

    chore: add generic

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,24 @@ fn main() {
     if let Some(3) = some_u8_value {
         println!("three");
     }
+
+    let p = Point { x: 5, y: 10 };
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+    let p3 = p1.mixup(p2);
+    print!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+struct Point<T, U> {
+    x: T,
+    y: U,
 }
